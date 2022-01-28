cdbe8de3b6ce320e40613db151f8a5ab19df0f45
Giovanni Reinoza <giovanni.reinoza@tegraglobal>
2022-01-28 16:42:43 -0600

    first commit, Im one with the force and the force is with me

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {  
+  // Return the App component.
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Mr Anderson welcome back
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
