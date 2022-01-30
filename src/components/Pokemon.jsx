9e94b0f443a8079ba95d592e353921c266d42e67
Giovanni Reinoza <giovanni.reinoza@tegraglobal>
2022-01-29 22:13:58 -0600

    improving pokemon component

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import pokeball from "../assets/images/pokeball.png"
 
-const Pokemon = ({name, image}) =>{
+export default function Pokemon (){
+  const [pokemon, setPokemon] = useState({id:0});
+
+  useEffect(()=>{
+    let random = Math.floor(Math.random() * 150) + 1
+    let url = "https://pokeapi.co/api/v2/pokemon/" + random;
+    fetch(url)
+    .then((res)=>res.json())
+    .then((json) => {
+      let pokemonResult = {
+        id: json.id,
+        name: json.name,
+        image: json.sprites.front_default
+      }
+      setPokemon(pokemonResult)
+    })
+  },[])
     return(
-      <figure className="col-1">
-        <img className="rounded float-right" src={image} alt={name} height="80px" title={name} />
-      </figure>
+      <React.Fragment>
+        {pokemon.id ===0 ? (
+          <figure className="col-1">
+          <img className="rounded float-right" src={pokeball} height="80px" title="Loading pokemon" />
+        </figure>
+        ):(
+          <figure className="col-1">
+            <img className="rounded float-right" src={pokemon.image} alt={pokemon.name} height="80px" title={pokemon.name} />
+          </figure>
+        )}
+      </React.Fragment>
     )
   }
-  export default Pokemon
+  
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,24 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Pokemon from "./Pokemon";
 
 export default function Todo(){
   const [task, setTask] = useState([]);
-  const [pokemon, setPokemon] = useState({id:0});
-
-  useEffect(()=>{
-    let random = Math.floor(Math.random() * 150) + 1
-    let url = "https://pokeapi.co/api/v2/pokemon/" + random;
-    fetch(url)
-    .then((res)=>res.json())
-    .then((json) => {
-      let pokemonResult = {
-        id: json.id,
-        name: json.name,
-        image: json.sprites.front_default
-      }
-      setPokemon(pokemonResult)
-    })
-  },[])
+  
 
   const handleKeyPress = (event) => {
     if(event.key === 'Enter'){
@@ -28,11 +13,7 @@ export default function Todo(){
 
   return(
     <div className="row">
-      {pokemon.id ===0 ? (
-        <h4>Loading pokemon</h4>
-      ):(
-        <Pokemon key={pokemon.id} name={pokemon.name} image={pokemon.image}/>
-      )}
+      <Pokemon />
       <h1 className="col-2">
         Todo List
       </h1>      
